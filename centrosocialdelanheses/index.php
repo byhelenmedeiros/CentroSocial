bb09597c3aa83c160788c5d97afef8f5f169b3e3
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2024-01-13 17:32:57 +0000

    inserir css e screanshot

---
+++ centrosocialdelanheses/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+
+    <!-- title -->
+    <title><?php bloginfo('name'); ?> - <?php bloginfo('description'); ?></title>
+
+    <!-- favicon -->
+    <link rel="shortcut icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.png">
+
+    <!-- google font -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/all.min.css">
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/bootstrap/css/bootstrap.min.css">
+
+    <!-- owl carousel -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/owl.carousel.css">
+
+    <!-- magnific popup -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/magnific-popup.css">
+
+    <!-- animate css -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/animate.css">
+
+    <!-- mean menu css -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/meanmenu.min.css">
+
+    <!-- main style -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/main.css">
+
+    <!-- responsive -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/responsive.css">
+
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+   
