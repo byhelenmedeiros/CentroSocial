1d7a759e8624bfbe6ed37fe9242d36804749d813
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2024-01-14 12:33:36 +0000

    change index

--- centrosocialdelanheses/index.php
+++ centrosocialdelanheses/index.php
@@ -16,28 +16,13 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
 
-    <!-- fontawesome -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/all.min.css">
-
-    <!-- bootstrap -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/bootstrap/css/bootstrap.min.css">
-
-    <!-- owl carousel -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/owl.carousel.css">
-
-    <!-- magnific popup -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/magnific-popup.css">
-
-    <!-- animate css -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/animate.css">
-
-    <!-- mean menu css -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/meanmenu.min.css">
-
-    <!-- main style -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/main.css">
-
-    <!-- responsive -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/responsive.css">
 
     </head>
