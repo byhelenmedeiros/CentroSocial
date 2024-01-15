b22cb4accda5b0a4bd3e91fb81b7864f572392d1
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2024-01-15 20:01:02 +0000

    change form contact

--- centrosocialdelanheses/components/contacto-content.php
+++ centrosocialdelanheses/components/contacto-content.php
@@ -1,113 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Responsive Bootstrap4 Shop Template, Created by Imran Hossain from https://imransdesign.com/">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/libphonenumber-js/1.7.44/libphonenumber-js.min.js"></script>
 
-	<!-- title -->
-	<title>Contact</title>
-
-	<!-- favicon -->
-	<link rel="shortcut icon" type="image/png" href="assets/img/favicon.png">
-	<!-- google font -->
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
-	<!-- fontawesome -->
-	<link rel="stylesheet" href="assets/css/all.min.css">
-	<!-- bootstrap -->
-	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-	<!-- owl carousel -->
-	<link rel="stylesheet" href="assets/css/owl.carousel.css">
-	<!-- magnific popup -->
-	<link rel="stylesheet" href="assets/css/magnific-popup.css">
-	<!-- animate css -->
-	<link rel="stylesheet" href="assets/css/animate.css">
-	<!-- mean menu css -->
-	<link rel="stylesheet" href="assets/css/meanmenu.min.css">
-	<!-- main style -->
-	<link rel="stylesheet" href="assets/css/main.css">
-	<!-- responsive -->
-	<link rel="stylesheet" href="assets/css/responsive.css">
-
-</head>
-<body>
-	
-	<!--PreLoader-->
-    <div class="loader">
-        <div class="loader-inner">
-            <div class="circle"></div>
-        </div>
-    </div>
-    <!--PreLoader Ends-->
-	
-	<!-- header -->
-	<div class="top-header-area" id="sticker">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12 col-sm-12 text-center">
-					<div class="main-menu-wrap">
-						<!-- logo -->
-						<div class="site-logo">
-							<a href="index.html">
-								<img src="assets/img/logo.png" alt="">
-							</a>
-						</div>
-						<!-- logo -->
-
-						<!-- menu start -->
-						<nav class="main-menu">
-							<ul>
-								<li><a href="index.html">Home</a>
-								</li>
-								<li><a href="quem somos.html">Quem Somos</a></li>
-								<li><a href="#">Respostas Social</a>
-									<ul class="sub-menu">
-										<li><a href="creche.html">Creche</a></li>
-										<li><a href="centrododia.html">Centro do dia</a></li>
-										<li><a href="residencia.html">Estrutura Residencial Para Pessoas Idosas</a></li>
-										<li><a href="adomicilio.html">Serviço de Apoio Domiciliário</a></li>
-									</ul>
-								</li>
-								<li><a href="trasparencia.html">Transparencia</a>
-								</li>
-								<li><a href="contactos.html">Contactos</a></li>
-								<li>
-									<div class="header-icons">
-										<a class="mobile-hide search-bar-icon" href="#"><i class="fas fa-search"></i></a>
-									</div>
-								</li>
-							</ul>
-						</nav>
-						<a class="mobile-show search-bar-icon" href="#"><i class="fas fa-search"></i></a>
-						<div class="mobile-menu"></div>
-						<!-- menu end -->
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- end header -->
-
-	<!-- search area -->
-	<div class="search-area">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12">
-					<span class="close-btn"><i class="fas fa-window-close"></i></span>
-					<div class="search-bar">
-						<div class="search-bar-tablecell">
-							<h3>Search For:</h3>
-							<input type="text" placeholder="Keywords">
-							<button type="submit">Search <i class="fas fa-search"></i></button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- end search arewa -->
 	
 	<!-- breadcrumb-section -->
 	<div class="breadcrumb-section breadcrumb-bg">
@@ -134,7 +26,7 @@
 				<h2>Candidate-se</h2>
 				<p>Para fazer parte de uma de nossas respostas sociais, preenche o nosso questionário.</p>
 			  </div>
-			  <h6>DADOS DE IDENTIFICAÇÃO DO(A) CANDIDATO(A):</h6>
+			  <h3>DADOS DE IDENTIFICAÇÃO DO(A) CANDIDATO(A):</h3>
 							  <div id="form_status"></div>
 							  <div class="contact-form">
 								<form type="POST" id="personal-info-form" onSubmit="return valid_datas( this );">
@@ -148,7 +40,7 @@
 								  </p>
 								  <p>
 									<input type="text" placeholder="Distrito" name="distrito" id="distrito">
-									<input type="date" placeholder="Data de Nascimento" name="dob" id="dob">
+									<input type="date" placeholder="Data de Nascimento" name="dob" id="data">
 								  </p>
 								  <p>
 									<input type="text" placeholder="Nº CC / B.I." name="idNumber" id="idNumber">
@@ -158,20 +50,28 @@
 									<input type="text" placeholder="NISS" name="niss" id="niss">
 									<input type="text" placeholder="SNS" name="sns" id="sns">
 								  </p>
+								  <hr>
+								  <h4>ESCOLHA O SERVIÇO:</h4>
 					<select id="options" onchange="showFields()">
-					  <option value="option1">Centro do Dia</option>
-					  <option value="option2">Residencia para Idosos</option>
-					  <option value="option3">Apoio Domiciliário</option>
-					  <option value="option4">Creche</option>
+					<div class="select-selected">Selecione uma opção</div>
+  						<div class="select-items">
+						<option value="" selected disabled>Escolha uma opção</option>
+						<option value="option1">Centro do Dia</option>
+					  	<option value="option2">Residencia para Idosos</option>
+					  	<option value="option3">Apoio Domiciliário</option>
+					  	<option value="option4">Creche</option>
 					</select>
+						
 				  </p>
+
 				  
 				  <div id="option1Fields" style="display:none;">
-					<!-- Campos específicos para la opción 1 -->
 					<p>
 						<p>
+							.	
+
 							<input type="text" placeholder="Nome completo" name="name" id="name">
-							<input type="tel" placeholder="telemovel" name="phone" id="phone">
+							<input type="tel" placeholder="Telemóvel" name="phone" id="phone">
 							<input type="email" placeholder="Email" name="email" id="email">
 							</p>
 							<p>
@@ -349,7 +249,7 @@
 							<p> Secretaria: <br> SEG-SEX: 09:30 - 17:00 </p>
 						</div>
 						<div class="contact-form-box">
-							<h4><i class="fas fa-address-book"></i> Contaccto</h4>
+							<h4><i class="fas fa-address-book"></i> Contacto</h4>
 							<p>Telefone: 258 739 000 <br> Email:geral@cpsribalima.pt</p>
 						</div>
 					</div>
@@ -388,110 +288,3 @@
 	</div>
 	<!-- end google map section -->
 
-
-	<!-- footer -->
-	<div class="footer-area">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-3 col-md-6">
-					<div class="footer-box about-widget">
-						<h2 class="widget-title">Centro Paroquial e Social Lanheses</h2>
-						
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6">
-					<div class="footer-box get-in-touch">
-						<h2 class="widget-title">Contactos</h2>
-						
-						<ul>
-							
-							<li>Morada:</li>
-							<li>Estrada da Igreja, nº468 4925-416 Lanheses</li>
-							<li>Email:</li>
-							<li>geral@cpsribalima.pt</li>
-							<li>Telefone:</li>
-							<li>258 739 000</li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6">
-					<div class="footer-box pages">
-						<h2 class="widget-title">Paginas</h2>
-						<ul>
-							<li><a href="index.html">Home</a></li>
-							<li><a href="quem somos.html">Quem Somos</a></li>
-							<!-- Não Sabemos como fazer para que fique melhor -->
-							<li><a href="#">Respostas Social</a>
-								<ul class="sub-menu">
-									<li><a href="creche.html">Creche</a></li>
-									<li><a href="centrododia.html">Centro do dia</a></li>
-									<li><a href="residencia.html">Estrutura Residencial Para Pessoas Idosas</a></li>
-									<li><a href="adomicilio.html">Serviço de Apoio Domiciliário</a></li>
-								</ul>
-							</li>
-							<li><a href="trasparencia.html">Transparencia</a></li>
-							<li><a href="contactos.html">Contacto</a></li>
-						</ul>
-						
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6">
-					<div class="footer-box subscribe">
-						<h2 class="widget-title">Candidatura</h2>
-						<p>Coloca teu email para mais</p>
-						<form action="index.html">
-							<input type="email" placeholder="Email">
-							<button type="submit"><i class="fas fa-paper-plane"></i></button>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- end footer -->
-	
-	<!-- copyright -->
-	<div class="copyright">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6 col-md-12">
-					<p>Copyrights &copy; 2015 - <a href="https://imransdesign.com/"> CPS Lanheses .</a>Todos os direitos reservados<br>
-					</p>
-				</div>
-				<div class="col-lg-6 text-right col-md-12">
-					<div class="social-icons">
-						<ul>
-							<li><a href="https://www.facebook.com/profile.php?id=281140885344385&paipv=0&eav=AfYOSUiZOIoLV0PV5X70FkrAmiWGYaPN56GN8oFqghp_6A_tiErZ9JDCxFEQskgbzxo&_rdr" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- end copyright -->
-	
-	<!-- jquery -->
-	<script src="assets/js/jquery-1.11.3.min.js"></script>
-	<!-- bootstrap -->
-	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
-	<!-- count down -->
-	<script src="assets/js/jquery.countdown.js"></script>
-	<!-- isotope -->
-	<script src="assets/js/jquery.isotope-3.0.6.min.js"></script>
-	<!-- waypoints -->
-	<script src="assets/js/waypoints.js"></script>
-	<!-- owl carousel -->
-	<script src="assets/js/owl.carousel.min.js"></script>
-	<!-- magnific popup -->
-	<script src="assets/js/jquery.magnific-popup.min.js"></script>
-	<!-- mean menu -->
-	<script src="assets/js/jquery.meanmenu.min.js"></script>
-	<!-- sticker js -->
-	<script src="assets/js/sticker.js"></script>
-	<!-- form validation js -->
-	<script src="assets/js/form-validate.js"></script>
-	<!-- main js -->
-	<script src="assets/js/main.js"></script>
-	
-</body>
-</html>
